c3d5396fbf600babaea529e2583a958f79018f76
KY-2187 <58202175+KY-2187@users.noreply.github.com>
2020-06-15 16:01:02 +0800

    edited

--- Lab1-Review/heroes/urls.py
+++ Lab1-Review/heroes/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 
-from.views import HeroesView
+from.views import HeroesView, JesterView
 
 
 urlpatterns = [
 	url(r'^/heroes', HeroesView.as_view(), name='heroes_list')
+	url(r'^/hero/jester', JesterView.as_view(), name='jester_detail')
 ]
+
